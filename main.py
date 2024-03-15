e47f32b37a32140c433f22ab5ef8b4e8e7f792b2
Tushar Khete <tusharkhete118@gmail.com>
2024-03-15 22:11:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from streamlit_ace import st_ace
 from pdfplumber import PDF
 from docx import Document as DocxDocument
 import textract
+import pandas as pd
 from features import process_document, process_webpage, display_chat_history
+from readcsv import process_csv
 import requests
 from io import BytesIO
 
@@ -13,7 +15,7 @@ os.environ["OPENAI_API_KEY"] = ""
 def main():
     st.title("Document Query App")
 
-    upload_option = st.radio("Choose input method", ["Upload File", "Enter URL"])
+    upload_option = st.radio("Choose input method", ["Upload File", "Enter URL", "Upload CSV"])
 
     chat_history = []
 
@@ -37,6 +39,12 @@ def main():
                 process_document(pdf_file, chat_history)
             else:
                 process_webpage(url, chat_history)
+    elif upload_option == "Upload CSV":
+        csv_file = st.file_uploader("Upload a CSV file", type="csv")
+        if csv_file is not None:
+            df = pd.read_csv(csv_file)
+            process_csv(df, chat_history)
+
 
     display_chat_history(chat_history)
 
